5e71bb68fb4e80e4cd976f4c054db18c7fe8d228
suma <kgsumakg30@gmail.com>
2024-03-11 17:20:00 +0530

    first commit

---
+++ index1.js
@@ -0,0 +1,12 @@
+const whiteImg=()=>{
+   document.getElementById("myImageON").style.display="none"
+   document.getElementById("myImageOFF").style.display="block"
+   document.getElementById("buttonM").style.color="green"
+}
+const yellowImg=()=>{
+    document.getElementById("myImageON").style.display="block"
+    document.getElementById("myImageOFF").style.display="none"
+    document.getElementById("buttonS").style.color="red"
+}
+
+
